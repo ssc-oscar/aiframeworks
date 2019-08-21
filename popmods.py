e500dd30ee3b9b476bf4e7a787bf277f94c583a5
dkennard3 <46568557+dkennard3@users.noreply.github.com>
2019-08-21 12:00:24 -0400

    Changed to using gzip module instead of subprocess

--- popmods.py
+++ popmods.py
@@ -1,5 +1,5 @@
-import subprocess
 import sys
+import gzip
 '''
 Script file that creates <module name>.first files which lists
 repositories and the first time(in Unix seconds) that a commit imported a given module.
@@ -64,25 +64,21 @@ elif lang == "swift":
 times = []
 for i in range(32):
 	print("Reading gz file number " + str(i))
-	command = "zcat /data/play/" + dir_lang + "thruMaps/c2bPtaPkgO" + dir_lang + "." + str(i) + ".gz"
-	p1 = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
-	p2 = subprocess.Popen("egrep -w " + module, shell=True, stdin=p1.stdout, stdout=subprocess.PIPE) 
-	output = p2.communicate()[0]
+	file = gzip.open("/data/play/" + dir_lang + "thruMaps/c2bPtaPkgO" + dir_lang + "." + str(i) + ".gz")
 
-	if output != "":
-		for entry in str(output).rstrip('\n').split("\n"):
-			entry = str(entry).split(";")
-			repo, time = entry[1], entry[2]
-			if time in times:
-				continue
-			else:
-				times.append(time)
-			if repo not in dict.keys() or time < dict[repo]:
-				for word in entry[5:]:
-					if module in word:
-						dict[repo] = time
-						break
-	
+	for line in file.readlines():
+		entry = str(line).split(";")
+		repo, time = entry[1], entry[2]
+		if time in times:
+			continue
+		else:
+			times.append(time)
+		if repo not in dict.keys() or time < dict[repo]:
+			for word in entry[5:]:
+				if module in word:
+					dict[repo] = time
+					break
+	file.close()
 first_file = module + ".first"
 f = open(first_file, "w")
 
